061f2672e17861bc56573cc7471d2f6118eb8c59
traydee <traydee92@gmail.com>
2025-06-19 22:29:43 +0200

    first commit

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-fetch("https://script.google.com/macros/s/AKfycbxplvQPVt_IIyJXPEoPNlS1SXBNuHmQOvr5xwUqy9zJEX0xtyF2RvsZxRjCK94evoAlvw/exec", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: new URLSearchParams({
-    action: "addFriends",
-    user_id: 5744864118,
-    value: 1
-  })
-})
-.then(response => response.text())
-.then(text => {
-  console.log("Ответ:", text);
-})
-.catch(error => {
-  console.error("Ошибка:", error);
-});
+// fetch("https://script.google.com/macros/s/AKfycbxplvQPVt_IIyJXPEoPNlS1SXBNuHmQOvr5xwUqy9zJEX0xtyF2RvsZxRjCK94evoAlvw/exec", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/x-www-form-urlencoded"
+//   },
+//   body: new URLSearchParams({
+//     action: "addFriends",
+//     user_id: 5744864118,
+//     value: 1
+//   })
+// })
+// .then(response => response.text())
+// .then(text => {
+//   console.log("Ответ:", text);
+// })
+// .catch(error => {
+//   console.error("Ошибка:", error);
+// });
 
 fetch("https://script.google.com/macros/s/AKfycbxplvQPVt_IIyJXPEoPNlS1SXBNuHmQOvr5xwUqy9zJEX0xtyF2RvsZxRjCK94evoAlvw/exec", {
   method: "POST",
@@ -23,7 +23,7 @@ fetch("https://script.google.com/macros/s/AKfycbxplvQPVt_IIyJXPEoPNlS1SXBNuHmQOv
     "Content-Type": "application/x-www-form-urlencoded"
   },
   body: new URLSearchParams({
-    action: "get_value",
+    action: "minusValue",
     user_id: 5744864118
   })
 })
